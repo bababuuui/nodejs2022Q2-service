6b1fc1e8b8cba2ba939b34bd98f134509bb995fb
bababuuui <bubinatar@gmail.com>
2022-07-14 20:15:21 +0200

    feat - albums

---
+++ src/albums/albums.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateAlbumDto } from './dto/create-album.dto';
+import { UpdateAlbumDto } from './dto/update-album.dto';
+import { Album } from '../albums/entities/album.entity';
+import { InMemoryStore } from '../db/in-memory-store';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class AlbumsService {
+  create(createAlbumDto: CreateAlbumDto) {
+    const album = new Album();
+    album.id = uuid();
+    album.name = createAlbumDto.name;
+    album.year = createAlbumDto.year;
+    album.artistId = createAlbumDto.artistId || null;
+
+    console.log(createAlbumDto);
+    InMemoryStore.albums.push(album);
+    return album;
+  }
+
+  findAll() {
+    return InMemoryStore.albums;
+  }
+
+  findOne(id: string) {
+    const album = InMemoryStore.albums.find((item) => item.id === id);
+    if (!album) {
+      throw new NotFoundException();
+    } else {
+      return album;
+    }
+  }
+
+  update(id: string, updateAlbumDto: UpdateAlbumDto) {
+    const album = this.findOne(id);
+    //update in db
+    const index = InMemoryStore.albums.indexOf(album);
+    album.name = updateAlbumDto.name;
+    album.year = updateAlbumDto.year;
+    album.artistId = updateAlbumDto.artistId;
+    InMemoryStore.albums[index] = album;
+    return album;
+  }
+
+  remove(id: string) {
+    const album = this.findOne(id);
+    if (album) {
+      InMemoryStore.albums = InMemoryStore.albums.filter((item) => item.id !== id);
+    }
+  }
+}
--- src/db/in-memory-store.ts
+++ src/db/in-memory-store.ts
@@ -1,9 +1,10 @@
 import { User } from '../users/entities/user';
 import { Artist } from '../artists/entities/artist.entity';
+import { Album } from '../albums/entities/album.entity';
 
 export class InMemoryStore {
   public static users: User[] = [];
   public static tracks: User[] = [];
-  public static albums: User[] = [];
+  public static albums: Album[] = [];
   public static artists: Artist[] = [];
 }
