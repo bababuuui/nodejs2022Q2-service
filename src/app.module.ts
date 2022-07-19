74747c3bcf584ed688a5de1254fbc75ac6cbc09d
bababuuui <bubinatar@gmail.com>
2022-07-19 23:35:05 +0200

    refactor - use typeORM for users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,20 @@ import { ArtistsModule } from './artists/artists.module';
 import { TracksModule } from './tracks/tracks.module';
 import { AlbumsModule } from './albums/albums.module';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configService from 'ormconfig';
 
 @Module({
-  imports: [UsersModule, ArtistsModule, TracksModule, AlbumsModule, FavoritesModule],
+  imports: [
+    UsersModule,
+    ArtistsModule,
+    TracksModule,
+    AlbumsModule,
+    FavoritesModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    TypeOrmModule.forRoot(configService),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
