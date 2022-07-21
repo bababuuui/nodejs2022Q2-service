214f2aaa50bf85454b16a3a991b3abbfcea43634
bababuuui <bubinatar@gmail.com>
2022-07-21 22:19:18 +0200

    fix - update orm config

--- ormconfig.ts
+++ ormconfig.ts
@@ -7,13 +7,13 @@ console.log(process.env.POSTGRES_DB);
 
 export default {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   entities: ['dist/**/entities/*{.ts,.js}'],
   synchronize: true,
-  migrations: ['dist/**/migrations/**/*{.ts,.js}'],
+  // migrations: ['dist/**/migrations/**/*{.ts,.js}'],
   // migrationsRun: true,
 } as DataSourceOptions;
