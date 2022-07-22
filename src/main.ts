8aa7dd404f6d58e61842ac9570a33e3f0625e384
bababuuui <bubinatar@gmail.com>
2022-07-22 18:50:44 +0200

    fix path to doc

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const rootDirname = dirname(__dirname);
-  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const DOC_API = await readFile(join(rootDirname, '..', 'doc', 'api.yaml'), 'utf-8');
   const document = parse(DOC_API);
   SwaggerModule.setup('doc', app, document);
 
