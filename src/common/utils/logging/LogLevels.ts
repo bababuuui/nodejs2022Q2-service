e0fc7e56ed2fcff3ef63b99d6acce8467fd6a49b
bababuuui <bubinatar@gmail.com>
2022-08-04 22:04:52 +0200

    feat - custom logger service with log levels

---
+++ src/common/utils/logging/LogLevels.ts
@@ -0,0 +1,5 @@
+export enum LogLevels {
+  ERROR = '0',
+  LOG = '1',
+  DEBUG = '2',
+}
---
+++ src/common/utils/logging/custom-logger.service.ts
@@ -0,0 +1,33 @@
+import { ConsoleLogger, Injectable, Logger, LoggerService, Scope } from '@nestjs/common';
+import * as fs from 'fs';
+import { ConsoleLoggerOptions } from '@nestjs/common/services/console-logger.service';
+import { LogLevel } from 'ts-loader/dist/logger';
+import { LogLevels } from './LogLevels';
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class CustomLogger extends ConsoleLogger {
+  constructor(context?: string, options?: ConsoleLoggerOptions) {
+    super(context, options);
+    //setting log levels
+    const levels = [];
+    const LEVEL = process.env.LOG_LEVEL || 0;
+    Object.keys(LogLevels).forEach((key) => {
+      console.log(LogLevels[key]);
+      if (parseInt(LogLevels[key]) <= LEVEL) {
+        levels.push(key.toLowerCase());
+      }
+    });
+    this.setLogLevels(levels);
+  }
+
+  log(message: any, ...optionalParams: any[]) {
+    super.log(message, ...optionalParams);
+  }
+
+  error(message: any, ...optionalParams: any[]) {
+    super.error(message, ...optionalParams);
+  }
+  debug(message: any, ...optionalParams: any[]) {
+    super.debug(message, ...optionalParams);
+  }
+}
